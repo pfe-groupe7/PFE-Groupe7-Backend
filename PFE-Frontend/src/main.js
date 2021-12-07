dca8c0f91aa15192f8dabe9ff74611f9e39d5a76
Chih Huai Lin <chihhuai.lin@student.vinci.be>
2021-12-07 19:21:43 +0100

    CI test commit

--- PFE-Frontend/src/main.js
+++ PFE-Frontend/src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './axios';
-import store from './vuex'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./axios";
+import store from "./vuex";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store, 
-  render: h => h(App),
-}).$mount('#app')
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
+
+//test1
