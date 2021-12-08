42c916e2a80877aa4ddfc9e5b5290daf8f91d78d
ikbouali <ikram.bouali@student.vinci.be>
2021-12-08 19:02:55 +0100

    change login

--- PFE-Frontend/src/components/Register.vue
+++ PFE-Frontend/src/components/Register.vue
@@ -2,50 +2,35 @@
   <form @submit.prevent="handleSubmit">
     <error v-if="error" :error="error" />
 
-    <h3>S'inscrire</h3>
+  <div class="login-wrap p-3 md-5">
+    <div class="d-flex">
+      <div class="w-100">
+        <h3>S'inscrire</h3>
+      </div>
+    </div>
+  </div>
 
-    <div class="form-group">
-      <label>Prénom</label>
-      <input
-        type="text"
-        class="form-control"
-        v-model="Prénom"
-        placeholder="Prénom"
-      />
-    </div>
+  <div class="form-group">
+    <input type="text" class="form-control" required="" v-model="Prénom" placeholder="Prénom"/>
+  </div>
 
-    <div class="form-group">
-      <label>Nom</label>
-      <input type="text" class="form-control" v-model="Nom" placeholder="Nom" />
+  <div class="form-group mt-3">
+    <input type="text" class="form-control" required="" v-model="Nom" placeholder="Nom" />
+  </div>
+  <div class="form-group mt-3">
+      <input type="email" class="form-control" v-model="email" placeholder="Email" />
+  </div>
+    <div class="form-group mt-3">
+      <input type="email" class="form-control" v-model="Password" placeholder="Mot de passe" />
+      <span toggle="#password-field" class="fa fa-fw field-icon toggle-password fa-eye"></span>
+
     </div>
-    <div class="form-group">
-      <label>Email</label>
-      <input
-        type="email"
-        class="form-control"
-        v-model="email"
-        placeholder="Email"
-      />
-    </div>
-    <div class="form-group">
-      <label>Password</label>
-      <input
-        type="password"
-        class="form-control"
-        v-model="password"
-        placeholder="Mot de passe"
-      />
-    </div>
-    <div class="form-group">
-      <label>Confirm Password</label>
-      <input
-        type="password"
-        class="form-control"
-        v-model="Confirm_Password"
-        placeholder="Confirmer le mot de passe"
-      />
-    </div>
-    <button class="btn btn-primary btn-block">S'inscrire</button>
+   <div class="form-group mt-3">
+      <input type="email" class="form-control" v-model="Confirm_Password" placeholder="Confirmez votre mot de passe" />
+  </div>
+   <div class="form-group mt-4">
+    <button type="submit" class="form-control btn btn-primary rounded submit px-3">S'inscrire</button>
+  </div>
   </form>
 </template>
 
