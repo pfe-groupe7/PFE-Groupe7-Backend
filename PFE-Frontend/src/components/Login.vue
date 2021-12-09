75bcfa51bd6b012100a0f5307a36c20ee18fe7ba
Abdulrazzak-ipl <abdulrssak@gmail.com>
2021-12-09 16:09:37 +0100

    add createAd

--- PFE-Frontend/src/components/Login.vue
+++ PFE-Frontend/src/components/Login.vue
@@ -14,11 +14,12 @@
             <div class="section text-center">
             <h4 class="mb-4 pb-4">Connexion</h4>
             <div class="form-group">
-              <input type="email" name="email" class="form-style" placeholder="Adresse e-mail" id="email" >
+              <input type="email" name="email" class="form-style" placeholder="Adresse e-mail" id="email" v-model="emailLogin">
               <i class="input-icon fa fa-envelope"></i>
             </div>	
             <div class="form-group mt-4">
-              <input type="password" name="password" class="form-style" placeholder="Mot de passe" id="password" >
+              <input type="password" name="password" class="form-style" placeholder="Mot de passe" id="password" v-model="mdpLogin"
+>
               <i class="input-icon fa fa-lock"></i>
             </div>
             <div class="form-group mt-4"> <a href="#" class="btn mt-3">Envoyer</a> </div>
@@ -32,24 +33,24 @@
           <div class="section text-center">
             <h4 class="mb-4 pb-3">Inscription</h4>
               <div class="form-group ">
-                <input type="text" name="nom" class="form-style" placeholder="Nom" id="nom">
+                <input type="text" name="nom" class="form-style" placeholder="Nom" id="nom" v-model="nomRegister">
               <i class="input-icon fa fa-user" ></i>
               </div>	
               <div class="form-group mt-3">
-                <input type="text" name="prenom" class="form-style" placeholder="Prénom" id="prenom" >
+                <input type="text" name="prenom" class="form-style" placeholder="Prénom" id="prenom" v-model="prenomRegister" >
               <i class="input-icon fa fa-user" ></i>
               </div>	
 
             <div class="form-group mt-3">
-              <input type="email" name="email" class="form-style" placeholder="Adresse e-mail" id="email" >
+              <input type="email" name="email" class="form-style" placeholder="Adresse e-mail" id="email" v-model="emailRegister">
               <i class="input-icon fa fa-envelope"></i>
             </div>
             <div class="form-group mt-3">
-              <input type="password" name="password" class="form-style" placeholder="Mot de passe" id="password" >
+              <input type="password" name="password" class="form-style" placeholder="Mot de passe" id="password" v-model="mdpRegister">
               <i class="input-icon fa fa-lock"></i>
             </div>
             <div class="form-group mt-3">
-              <select name="campus" id="campus" class="form-style">
+              <select name="campus" id="campus" class="form-style" v-model="campusRegister">
                 <option value="" disabled selected>Sélectionne ton campus</option>
                 <option value="0">Woluwe</option>
                 <option value="1">Ixelles</option>
