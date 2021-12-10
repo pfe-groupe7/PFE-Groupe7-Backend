06539b21da926a18773025614c7b6fe5bc9349af
Chih Huai Lin <chihhuai.lin@student.vinci.be>
2021-12-10 13:16:02 +0100

    add test and classes : Location, Campus

--- PFE-Backend/PFE/models.py
+++ PFE-Backend/PFE/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from django.db.models.base import Model, ModelState
+from django.db.models.deletion import CASCADE
 
+class Campus(models.Model):
+    id = models.AutoField(primary_key=True)
+    campusName = models.CharField('campus_name',unique=True,max_length=64)
+
+
+class Location(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField('name',max_length=256)
+    address = models.CharField('address',max_length=512)
+    campus = models.ForeignKey('campus',on_delete=CASCADE,default=1)
 
 
 class User(models.Model):
@@ -10,7 +22,7 @@ class User(models.Model):
     password = models.CharField('password', max_length=200)
 
     moderator = models.BooleanField('is_moderator', max_length=200)
-    campus = models.IntegerField('campus_id', default=1)
+    campus = models.ForeignKey('campus',on_delete=models.CASCADE,default=1)
 
 
 # Enums for Ad
@@ -34,4 +46,9 @@ class Ad(models.Model):
     state = models.CharField(
         max_length=64, choices=State.choices, default=State.PENDING)
     price = models.IntegerField('price')
-    seller_id = models.IntegerField('user_id', default=1)
+    seller = models.ForeignKey('user',on_delete=models.CASCADE,default=1)
+
+
+
+
+
